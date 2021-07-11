1e0742fbb8a6218069b514745b88565c6dbfe466
wensquared <71566436+wensquared@users.noreply.github.com>
2021-07-11 14:09:27 +0200

    composer install

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,11 +1,11 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '38178b4588e562a097c2e93bb3be43c190193d63',
         'name' => 'rowena/wifi',
         'dev' => true,
     ),
@@ -20,12 +20,12 @@
             'dev_requirement' => false,
         ),
         'rowena/wifi' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '38178b4588e562a097c2e93bb3be43c190193d63',
             'dev_requirement' => false,
         ),
         'wixel/gump' => array(
